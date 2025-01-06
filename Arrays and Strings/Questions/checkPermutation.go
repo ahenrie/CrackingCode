703afab293ac18c6e6fd1081dc41a3717193aa00
Ahenrie <10914763@uvu.edu>
2025-01-06 12:35:23 -0800

    Permutations

--- Arrays and Strings/Questions/checkPermutation.go
+++ Arrays and Strings/Questions/checkPermutation.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"slices"
+	"sort"
+)
+
+func arePermutations(string1 string, string2 string) bool {
+	// check length
+	if len(string1) != len(string2) {
+		return false
+	}
+
+	slice1 := []rune{}
+	slice2 := []rune{}
+
+	//convert strings to slices
+	for _, runes := range string1 {
+		slice1 = append(slice1, runes)
+	}
+	for _, runes := range string2 {
+		slice1 = append(slice2, runes)
+	}
+
+	sort.Slice(slice1, func(i, j int) bool {
+		return slice1[i] < slice1[j]
+	})
+
+	sort.Slice(slice2, func(i, j int) bool {
+		return slice2[i] < slice2[j]
+	})
+
+	return slices.Equal(slice1, slice2)
+}
